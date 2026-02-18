e1f11377752cb519214712bed887cc0470451e9f
Kim Gustafsson <kigu25@student.bth.se>
2026-02-18 14:15:00 +0100

    GameObject class

--- Software_lib/GameObject.h
+++ Software_lib/GameObject.h
@@ -1,1 +1,21 @@
-#pragma once
+# pragma once
+# include <string>
+
+using namespace std;
+
+class Gameobject
+{
+public:
+	Gameobject();
+	~Gameobject();
+
+	void listinteractionTypes();
+	void startinteraction(string theInteractionType);
+	void listCurrentInteractionOptions(string theOptions);
+	void setCurrentInteractionOptions(string theOptions);
+	void startCurrentInteraction();
+	void abortCurrentInteraction()
+
+private:
+	string objectName;
+};
