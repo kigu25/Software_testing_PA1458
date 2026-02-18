328dae88533f1c8a1c70f96c990b867f2338dddd
jath2025 <jath25@student.bth.se>
2026-02-18 14:21:26 +0100

    Scene functions

---
+++ Software_lib/Scene.cpp
@@ -0,0 +1,11 @@
+#include "Scene.h"
+
+void Scene::listAvailableElements() {
+
+}
+bool Scene::isAvailable(string gameElementName) {
+
+}
+bool Scene::isGameObject(string gameElementName) {
+
+}
--- Software_lib/Scene.h
+++ Software_lib/Scene.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <string>
+
+using namespace std;
+class Scene {
+private:
+
+public:
+	void listAvailableElements();
+	bool isAvailable(string gameElementName);
+	bool isGameObject(string gameElementName);
+ };
