a4657e41ebc3a7abf68efa97a89c553f310b274f
jath2025 <jath25@student.bth.se>
2026-02-18 14:39:51 +0100

    Added appropriate Game functions

---
+++ Software_lib/Game.cpp
@@ -0,0 +1,23 @@
+#include "Game.h"
+
+Game::Game() {
+
+}
+Game::~Game() {
+
+}
+void Game::selectGameObject(string name) {
+
+}
+void Game::selectInteraction(###, string theInteraction) {
+
+}
+void Game::setInteractionOptions(string theOptions) {
+
+}
+void Game::startInteraction() {
+
+}
+void Game::abortInteraction() {
+
+}
