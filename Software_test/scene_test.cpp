2f966a1b6f8579bbe28cac971a7ef6693018cf4c
Kim Gustafsson <kigu25@student.bth.se>
2026-03-03 15:58:02 +0100

    Test that checks that gameOBJ is actually a pointer added.

--- Software_test/scene_test.cpp
+++ Software_test/scene_test.cpp
@@ -1,16 +1,34 @@
 #include "pch.h"
 #include "../Software_lib/Scene.h"
 
-TEST(scene_test, scene_constructor) {
+
+TEST(scene_test, scene_standard_constructor) {
+
+	Scene scene1;
+
+	ASSERT_EQ(scene1.getName(), "");
+}
+
+TEST(scene_test, scene_name_constructor) {
 
 	Scene scene1("Library");
 
 	ASSERT_EQ(scene1.getName(), "Library");
 }
 
-TEST(scene_test, scene_with_gameOBJ) {
+
+TEST(scene_test, scene_available_gameOBJ) {
 
 	Scene scene1;
 
 	ASSERT_EQ(scene1.isAvailable("Door"), true);
+}
+
+
+TEST(scene_test, get_the_object) {
+
+	Scene scene1;
+	Gameobject* object = scene1.getTheObject("Door");
+
+	ASSERT_NE(object, nullptr); // Make sure the pointer is NOT nullptr (_NE)
 }
