95fc6f69174cfc310b7ae91408fa06aff3822ef2
Kim Gustafsson <kigu25@student.bth.se>
2026-03-05 16:46:06 +0100

    Inplemented some tests for gameObject.

--- Software_test/gameObject_test.cpp
+++ Software_test/gameObject_test.cpp
@@ -1,7 +1,18 @@
 #include "pch.h"
-#include "../Software_lib/Interactions.h"
+#include "../Software_lib/GameObject.h"
 
 
-TEST(GameObject_test, placeHolder) {
+TEST(GameObject_test, Constructor) {
 
+	Gameobject Obj("Door2");
+
+	ASSERT_EQ(Obj.getName(), "Door2");
+}
+
+
+TEST(GameObject_test, ListInteractionTypes) {
+
+	Gameobject Obj("Door2");
+
+	ASSERT_EQ(Obj.listinteractionTypes(), "Open");
 }
