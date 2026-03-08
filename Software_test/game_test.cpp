8c74df55ab27fb2fe3d322db89150d79c2d751f7
Kim Gustafsson <kigu25@student.bth.se>
2026-03-08 14:19:11 +0100

    Added negative tests for Game.

--- Software_test/game_test.cpp
+++ Software_test/game_test.cpp
@@ -10,8 +10,27 @@ TEST(game_test, select_gameObject) {
 	ASSERT_NE(Game.getCurrentObj(), nullptr);
 }
 
+TEST(game_test, get_currentOption) {
 
-TEST(game_test, select_gameObject_nonexsistent) {
+	Game Game;
+
+	EXPECT_EQ(Game.getCurrentOption(), "");
+
+}
+
+TEST(game_test, set_currentOption) {
+
+	Game Game;
+
+	Game.setInteractionOption("Lockpick");
+	EXPECT_EQ(Game.getCurrentOption(), "Lockpick");
+}
+
+
+
+
+// Negative path
+TEST(game_negative, select_gameObject_nonexsistent) {
 
 	Game Game;
 
@@ -19,3 +38,20 @@ TEST(game_test, select_gameObject_nonexsistent) {
 	EXPECT_EQ(Game.getCurrentObj(), nullptr);
 }
 
+
+TEST(game_negative, get_currentOption) {
+
+	Game Game;
+
+	EXPECT_NE(Game.getCurrentOption(), "Lockpick");
+
+}
+
+
+TEST(game_negative, set_currentOption) {
+
+	Game Game;
+
+	Game.setInteractionOption("Lockpick");
+	EXPECT_NE(Game.getCurrentOption(), "FlyingCar");
+}
