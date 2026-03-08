1ad83dc644861071e03114c3cd4af7ad40c9352c
jath2025 <jath25@student.bth.se>
2026-03-08 17:23:32 +0100

    PhoneBook skeleton

---
+++ Software_lib/PhoneBook.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class PhoneBook {
+private:
+
+public:
+	PhoneBook();
+	~PhoneBook();
+
+	string getContactMethod(string theCharacter);
+
+};
