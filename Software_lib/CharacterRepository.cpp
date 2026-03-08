0266febe4001db2cd38240fb2d88270e89485e2f
StudentErik25 <ertu25@student.bth.se>
2026-03-08 17:28:46 +0100

    CharacterRpository Skeleton

---
+++ Software_lib/CharacterRepository.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "CharacterRepository.h"
+
+string getCharacter(string theCharacter)
+{
+	return "You got the character";
+}
---
+++ Software_lib/CharacterRepository.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+class CharacterRepository
+{
+public:
+	string getCharacter(string theCharacter); //Change to Character later
+};
