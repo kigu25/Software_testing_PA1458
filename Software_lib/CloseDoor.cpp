42f7d7f53755709880aebf8bfa9d66a9250ab082
StudentErik25 <ertu25@student.bth.se>
2026-03-08 11:13:53 +0100

    Finished options for closedoor

--- Software_lib/CloseDoor.cpp
+++ Software_lib/CloseDoor.cpp
@@ -3,7 +3,8 @@
 
 closeDoor::closeDoor(string interactionName) : Interactions(interactionName)
 {
-    this->interaction = "The door is closed.";
+    options.push_back("Slam");
+    options.push_back("Gently");
 }
 
 closeDoor::~closeDoor()
@@ -12,17 +13,17 @@ closeDoor::~closeDoor()
 
 bool closeDoor::execute(string option)
 {
+    if (option == "Slam")
+    {
+        cout << "You've slammed the door shut." << endl;
+        return true;
+    }
 
-}
+    if (option == "Gently")
+    {
+        cout << "You've gently closed the door." << endl;
+        return true;
+    }
 
-string closeDoor::slamDoorShut()
-{
-    this->interaction = "You've slammed the door shut.";
-    return this->interaction;
-}
-
-string closeDoor::gentlyCloseDoor()
-{
-    this->interaction = "You've gently closed the door.";
-    return this->interaction;
+    return false;
 }
