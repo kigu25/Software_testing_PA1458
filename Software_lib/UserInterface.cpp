ee2318028fdea53a39ebe7b60d31f2c522dd6034
Kim Gustafsson <kigu25@student.bth.se>
2026-03-08 18:31:47 +0100

    userInterface

--- Software_lib/UserInterface.cpp
+++ Software_lib/UserInterface.cpp
@@ -2,16 +2,76 @@
 
 Userinterface::Userinterface()
 {
+	this->run = true;
+	this->menuChoice = -1;
 }
 
 Userinterface::~Userinterface()
 {
 }
 
+
+//This will be called form main to start
 void Userinterface::start()
 {
+
+	interFaceLoop();
 }
 
+
+//The loop itself
 void Userinterface::interFaceLoop()
 {
+	while (run = true)
+	{
+		clearScreen();
+
+		showMainMenu();
+		cout << '\n' << "What do you want to do? : ";
+		cin >> this->menuChoice;
+
+		menuChoiceHandler(menuChoice);
+
+	}
+}
+
+void Userinterface::showMainMenu()
+{
+
+	// ALl menuchoice put here.
+
+
+
+	cout << "0. Exit program" << endl;
+}
+
+
+// Handle user-choices
+void Userinterface::menuChoiceHandler(int menuChoice)
+{
+	switch (menuChoice)
+	{
+	case 0:
+		this->run = false;
+
+		break;
+
+	case 1:
+
+		break;
+	
+
+	case 2:
+
+		break;
+
+
+
+	}
+}
+
+void Userinterface::clearScreen()
+{
+
+	cout << "\033[3J\033[H\033[2J";
 }
