6993a048ad36fbbb129c2af3a1ea413be34bfbd2
jath2025 <jath25@student.bth.se>
2026-03-09 14:08:24 +0100

    CharacterRepository implemented with its methods

--- Software_lib/CharacterRepository.h
+++ Software_lib/CharacterRepository.h
@@ -1,9 +1,17 @@
 #pragma once
 #include <string>
+#include <vector>
+#include "Character.h"
 using namespace std;
+
 
 class CharacterRepository
 {
+private:
+	vector<Character*> characters;
 public:
-	string getCharacter(string theCharacter); //Change to Character later
+	CharacterRepository();
+	~CharacterRepository();
+
+	Character* getCharacter(string theCharacter);
 };
